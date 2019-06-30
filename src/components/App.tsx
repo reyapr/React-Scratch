3dec4bcfa1f8baf890ab81ee4b1b07e467e55510
Renal Apriansyah <renal@gmail.com>
2019-06-30 09:53:46 +0700

    add App component use tsx

---
+++ src/components/App.tsx
@@ -0,0 +1,17 @@
+import * as React from "react"
+
+export interface Props {}
+
+class App extends React.Component<Props> {
+  render() {
+    return (
+      <React.Fragment>
+        from TypeScript
+      </React.Fragment>
+    )
+  }
+}
+
+export default App
+
+
